129955088782e577ddf91d7b567ea0c139615b0e
toelapiut <toelapiut7@gmail.com>
2017-12-09 12:03:41 +0300

    added a view function for index

--- insta/instagram/views.py
+++ insta/instagram/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
 
 # Create your views here.
+
+def index(request):
+
+    title='Hello world'
+    return render(request, 'index.html', {"title": title})
