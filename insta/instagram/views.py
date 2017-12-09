dd86016a868a6dddc4b5d1f0aa3775f71b7579d4
toelapiut <toelapiut7@gmail.com>
2017-12-09 15:01:58 +0300

    fx bug

--- insta/instagram/views.py
+++ insta/instagram/views.py
@@ -10,34 +10,8 @@ import os
 
 # Create your views here.
 @login_required(login_url='/accounts/register')
-def timeline(request):
-    '''
-    View function to display the timeline page for an authenticated logged in user
-    '''
-    current_user = request.user
-
-    title = 'Home'
-
-    following = Follow.get_following(current_user.id)
-
-    posts = Post.get_posts()
-
-    following_posts = []
-
-    for follow in following:
-
-        for post in posts:
-
-            if follow.profile == post.profile:
-
-                following_posts.append(post)
-
-    return render(request, 'all-posts/timeline.html', {"title": title, "following": following, "user":current_user, "following_posts":following_posts})
-
 def index(request):
-    '''
-    View function to display the timeline page for an authenticated logged in user
-    '''
+
     current_user = request.user
 
     title='instagram'
