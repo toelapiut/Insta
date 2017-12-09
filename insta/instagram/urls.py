58fc12dea1fcc93dbca482a1afac126c706c48d1
toelapiut <toelapiut7@gmail.com>
2017-12-10 00:35:12 +0300

    corrected urls and named them approprately

--- insta/instagram/urls.py
+++ insta/instagram/urls.py
@@ -6,13 +6,14 @@ from . import views
 
 urlpatterns = [
     url( r'^$', views.index, name="timeline"),
-    url( r'^post', views.post, name="new-post"),
+    url( r'^new/post/$', views.post, name="new-post"),
     url( r'^profile/(\d+)', views.profile, name="profile"),
     url( r'^comment(\d+)', views.comment, name="comment" ),
     url( r'^following/(\d+)', views.follow, name="follow"),
     url( r'^look_up/(\d+)', views.look_up, name="look-up"),
-    url( r'^like/(\d+)', views.like, name="like"),
-    url( r'^post/(\d+)', views.post_look, name="post"),
+    url( r'^like/(\d+)', views.like, name="liker"),
+    url( r'^post/(\d+)', views.post_look, name="post_look"),
+
 ]
 
 if settings.DEBUG:
